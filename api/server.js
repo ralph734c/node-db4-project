5cf98840d213729b2b4bbfb135e044f34d7104da
Christopher Ralph <chris.734c@gmail.com>
2024-08-24 22:25:50 -0400

    📦 NEW: Initial project setup, directory structure, basic server, and knex/db config

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const RecipesRouter = require('./recipes/recipes-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'The API is running' });
+});
+
+module.exports = server;
